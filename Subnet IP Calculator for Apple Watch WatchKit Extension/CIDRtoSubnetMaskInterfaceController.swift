92a3d95c1eefeecce30677ee81280d546b454e7e
Apope1999 <Apope1999@gmail.com>
2019-11-03 23:41:34 +0200

    Reading pickers position from button

--- Subnet IP Calculator for Apple Watch WatchKit Extension/CIDRtoSubnetMaskInterfaceController.swift
+++ Subnet IP Calculator for Apple Watch WatchKit Extension/CIDRtoSubnetMaskInterfaceController.swift
@@ -17,14 +17,20 @@ class CIDRtoSubnetMaskInterfaceController: WKInterfaceController {
     var quartette_2: String?
     var quartette_3: String?
     var quartette_4: String?
-    let cidrCodes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+    let cidrCodes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
     var pickerItems: [WKPickerItem] = []
+    var pickerCurrentPos = 1
     
     @IBOutlet weak var pickerValue: WKInterfacePicker!
     
+    @IBAction func pickerChanged(_ value: Int) {
+        pickerCurrentPos = value + 1
+    }
     @IBOutlet weak var subnetMaskLabel: WKInterfaceLabel!
     
     @IBAction func calculateIsClicked() {
+        //TODO
+        print(pickerCurrentPos)
     }
     
     
@@ -33,11 +39,9 @@ class CIDRtoSubnetMaskInterfaceController: WKInterfaceController {
         
         // Configure interface objects here.
         // Configure picker
-        
-        
         for cidrCode in cidrCodes {
             let item = WKPickerItem()
-            item.title = String(cidrCode)
+            item.title = "/" + String(cidrCode)
             pickerItems.append(item)
         }
         pickerValue.setItems(pickerItems)
