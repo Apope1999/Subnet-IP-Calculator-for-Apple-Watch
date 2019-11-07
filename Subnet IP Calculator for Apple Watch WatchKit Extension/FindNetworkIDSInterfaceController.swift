2915b0600da75a6014ea231876730e76d8eac370
Apope1999 <Apope1999@gmail.com>
2019-11-07 23:43:05 +0200

    Created and linked Storyboard
    Linked the Storyboard of Find Network IDs
    Renamed a button from "Find Network Hosts" to "Find Network IDs"

---
+++ Subnet IP Calculator for Apple Watch WatchKit Extension/FindNetworkIDSInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  FindNetworkIDSInterfaceController.swift
+//  Subnet IP Calculator for Apple Watch WatchKit Extension
+//
+//  Created by Apostolos Pezodromou on 7/11/19.
+//  Copyright © 2019 Apostolos Pezodromou. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FindNetworkIDSInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
