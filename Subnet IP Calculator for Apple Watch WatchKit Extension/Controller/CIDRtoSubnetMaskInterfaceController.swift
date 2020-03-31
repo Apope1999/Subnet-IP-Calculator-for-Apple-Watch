c3953fb4c16d1a6e9bc2ac219c82faa5f65d90b0
Apope1999 <Apope1999@gmail.com>
2020-03-31 23:55:34 +0300

    Refactored code for CIDR to SubnetMask

---
+++ Subnet IP Calculator for Apple Watch WatchKit Extension/Controller/CIDRtoSubnetMaskInterfaceController.swift
@@ -0,0 +1,58 @@
+//
+//  CIDRtoSubnetMaskInterfaceController.swift
+//  Subnet IP Calculator for Apple Watch WatchKit Extension
+//
+//  Created by Apostolos Pezodromou on 3/11/19.
+//  Copyright © 2019 Apostolos Pezodromou. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class CIDRtoSubnetMaskInterfaceController: WKInterfaceController {
+
+    
+    let cidrCodes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+    var pickerItems: [WKPickerItem] = []
+    var pickerCurrentPos = 1
+    var manager = CIDRtoSubnetMaskManager()
+    
+    @IBOutlet weak var pickerValue: WKInterfacePicker!
+    @IBOutlet weak var subnetMaskLabel: WKInterfaceLabel!
+    
+    
+    @IBAction func pickerValueChanged(_ value: Int) {
+        pickerCurrentPos = value + 1
+    }
+    
+    @IBAction func calculateIsClicked() {
+        // Creates label for User Interface
+        let label = manager.calculateSubnetMask(forCIDR: pickerCurrentPos)
+        subnetMaskLabel.setText(label)
+    }
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+        // Configure picker
+        for cidrCode in cidrCodes {
+            let item = WKPickerItem()
+            item.title = "/" + String(cidrCode)
+            pickerItems.append(item)
+        }
+        pickerValue.setItems(pickerItems)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Subnet IP Calculator for Apple Watch WatchKit Extension/Model/CIDRtoSubnetMaskManager.swift
@@ -0,0 +1,69 @@
+//
+//  CIDRtoSubnetMaskManager.swift
+//  Subnet IP Calculator for Apple Watch WatchKit Extension
+//
+//  Created by Apostolos Pezodromou on 31/3/20.
+//  Copyright © 2020 Apostolos Pezodromou. All rights reserved.
+//
+
+import Foundation
+
+struct CIDRtoSubnetMaskManager {
+    var cidr: Int?
+    var binaryArray: [String] = []
+    var quartette_1 = ""
+    var quartette_2 = ""
+    var quartette_3 = ""
+    var quartette_4 = ""
+    
+    // Converts the CIDR into Binary
+    mutating func convertCIDRToBinary() {
+        if let safeCidr = cidr {
+            for _ in 1...safeCidr {
+                binaryArray.append("1")
+            }
+            
+            let zeros = 32 - safeCidr
+            if (zeros != 0) {
+                for _ in 1...zeros {
+                    binaryArray.append("0")
+                }
+            }
+        }
+    }
+    
+    mutating func splitBinaryToQuartette() {
+        for x in 0...7 {
+            quartette_1.append(binaryArray[x])
+        }
+        
+        for x in 8...15 {
+            quartette_2.append(binaryArray[x])
+        }
+        
+        for x in 16...23 {
+            quartette_3.append(binaryArray[x])
+        }
+        
+        for x in 24...31 {
+            quartette_4.append(binaryArray[x])
+        }
+    }
+    
+    mutating func calculateSubnetMask(forCIDR cidr: Int) -> String {
+        self.cidr = cidr
+        convertCIDRToBinary()
+        splitBinaryToQuartette()
+        let subnetMask: String = String(strtoul(quartette_1, nil, 2)) + "." + String(strtoul(quartette_2, nil, 2)) + "." + String(strtoul(quartette_3, nil, 2)) + "." + String(strtoul(quartette_4, nil, 2))
+        reset()
+        return subnetMask
+    }
+    
+    mutating func reset() {
+        quartette_1 = ""
+        quartette_2 = ""
+        quartette_3 = ""
+        quartette_4 = ""
+        binaryArray = []
+    }
+}
